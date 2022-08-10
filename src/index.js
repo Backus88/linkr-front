3d7d507b59912d9a6f286164270610bdaba829c7
Luis <lf.mc.almeida@poli.ufrj.br>
2022-08-10 20:07:36 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import App from './components/App.js'
+import './styles/reset.css'
 
-ReactDOM.render(<App />, document.querySelector(".root"))
+ReactDOM.render(<App />, document.querySelector(".root"));
