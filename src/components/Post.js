5e80bede339f3c7e2ae88e0f615eca3511c1a008
brualvess <oliveirabrunna2210@gmail.com>
2022-08-10 16:02:05 -0300

    layout /post

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,23 +2,30 @@ import GlobalStyle from "./globalStyles";
 import Header from './Header.js';
 import styled from "styled-components";
 
-export default function Post(){
-    return(
+export default function Post() {
+    return (
         <>
-        <GlobalStyle/>
-        <Header/>
-        <Container>
-        <Title>timeline</Title>
-        <Publication>
-        <ProfileImage></ProfileImage>
-        <ContainerPost>
-        <ShareHeader>What are you going to share today?</ShareHeader>
-        <input type='text' placeholder="http://..." />
-        <input className="input2" type='text' placeholder="Awesome article about #javascript"  />
-        <Button>Publish</Button>
-        </ContainerPost>
-        </Publication>
-        </Container>
+            <GlobalStyle />
+            <Header />
+            <Container>
+                <Title>timeline</Title>
+                <Publish>
+                    <ProfileImage></ProfileImage>
+                    <ContainerPost>
+                        <ShareHeader>What are you going to share today?</ShareHeader>
+                        <input type='text' placeholder="http://..." />
+                        <input className="input2" type='text' placeholder="Awesome article about #javascript" />
+                        <Button>Publish</Button>
+                    </ContainerPost>
+                </Publish>
+                <Publication className="post">
+                    <ProfileImage></ProfileImage>
+                    <ContainerPost>
+                        <UserName>Juvenal Juvênico</UserName>
+                        <DescriptionPost>Muito maneiro esse tutorial de Material UI com React, deem uma olhada!</DescriptionPost>
+                    </ContainerPost>
+                </Publication>
+            </Container>
         </>
     )
 }
@@ -37,7 +44,7 @@ color: #FFFFFF;
 margin-top: 100px;
 
 `
-const Publication = styled.div`
+const Publish = styled.div`
 width: 40%;
 height: 190px;
 margin-top: 40px;
@@ -91,7 +98,7 @@ margin-top: 5px;
 `
 const Button = styled.div`
 background: #1877F2;
-border-radius: 3px;
+border-radius: 5px;
 font-family: 'Lato';
 font-style: normal;
 font-weight: 600;
@@ -102,5 +109,31 @@ width:17%;
 padding: 5px;
 align-self: flex-end;
 margin-top: 10px;
-margin-right: 20px;
+margin-right: 80px;
+`
+const Publication = styled.div`
+width: 40%;
+height: 257px;
+margin-top: 40px;
+background: #171717;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 16px;
+`
+const UserName = styled.div`
+font-family: 'Lato';
+font-style: normal;
+font-weight: 600;
+font-size: 19px;
+color: #FFFFFF;
+margin-top: 15px;
+margin-left: 80px;
+`
+const DescriptionPost = styled.div`
+font-family: 'Lato';
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+color: #B7B7B7;
+margin-left: 80px;
+margin-top: 10px;
 `
