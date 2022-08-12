d06943089962682d8b07671de5923900d76f1042
brualvess <oliveirabrunna2210@gmail.com>
2022-08-12 20:49:14 -0300

    update import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from './components/App.js'
-import React from 'react'
+import React, {createRoot} from 'react'
 import './styles/reset.css'
 
 ReactDOM.createRoot(document.getElementById("root"),).render(<App/>);
